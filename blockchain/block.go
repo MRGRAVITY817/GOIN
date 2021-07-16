9290db0db7a66c6e07459d9adcc360aa0d4fe114
Seong Hoon Wee <mrgravity817@gmail.com>
2021-07-16 12:17:04 +0900

    ADD block method that checks difficulty and nonce

--- blockchain/block.go
+++ blockchain/block.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha256"
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/MRGRAVITY817/goin/db"
 	"github.com/MRGRAVITY817/goin/utils"
@@ -30,15 +31,31 @@ func (b *Block) restore(data []byte) {
 	utils.FromBytes(b, data)
 }
 
+func (b *Block) mine() {
+	target := strings.Repeat("0", b.Difficulty)
+	for {
+		blockAsString := fmt.Sprint(b)
+		hash := fmt.Sprintf("%x", sha256.Sum256([]byte(blockAsString)))
+		fmt.Printf("Block as string: %s\nHash: %s\nNonce: %d\nTarget: %s\n\n", blockAsString, hash, b.Nonce, target)
+		if strings.HasPrefix(hash, target) {
+			b.Hash = hash
+			break
+		} else {
+			b.Nonce++
+		}
+	}
+}
+
 func createBlock(data string, prevHash string, height int) *Block {
 	block := Block{
-		Data:     data,
-		Hash:     "",
-		PrevHash: prevHash,
-		Height:   height,
+		Data:       data,
+		Hash:       "",
+		PrevHash:   prevHash,
+		Height:     height,
+		Difficulty: difficulty,
+		Nonce:      0,
 	}
-	payload := block.Data + block.PrevHash + fmt.Sprint(block.Height)
-	block.Hash = fmt.Sprintf("%x", sha256.Sum256([]byte(payload)))
+	block.mine()
 	block.persist()
 	return &block
 }
--- main.go
+++ main.go
@@ -1,23 +1,11 @@
 package main
 
 import (
-	"crypto/sha256"
-	"fmt"
-	"strings"
+	"github.com/MRGRAVITY817/goin/cli"
+	"github.com/MRGRAVITY817/goin/db"
 )
 
 func main() {
-	difficulty := 5
-	target := strings.Repeat("0", difficulty)
-	nonce := 1
-	for {
-		ingred := sha256.Sum256([]byte("hello" + fmt.Sprint(nonce)))
-		hash := fmt.Sprintf("%x", ingred)
-		fmt.Printf("Hash: %s\nTarget: %s\nNonce: %d\n\n", hash, target, nonce)
-		if strings.HasPrefix(hash, target) {
-			return
-		} else {
-			nonce++
-		}
-	}
+	defer db.Close()
+	cli.Start()
 }
