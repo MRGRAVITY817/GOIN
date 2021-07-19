2fd6e439fe11f813f3bde136f7bd582da2d8f270
Seong Hoon Wee <mrgravity817@gmail.com>
2021-07-19 22:52:58 +0900

    ADD goroutines

--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
 import (
-	"github.com/MRGRAVITY817/goin/cli"
-	"github.com/MRGRAVITY817/goin/db"
+	"fmt"
+	"time"
 )
 
-func main() {
-	defer db.Close()
-	cli.Start()
+func countToTen(c chan int) {
+	for i := range [10]int{} {
+		time.Sleep(1 * time.Second)
+		fmt.Printf("Sending %d\n", i)
+		c <- i
+	}
+	close(c) // close channel after used
 }
+
+func receive(c chan int) {
+	for {
+		a, ok := <-c
+		if !ok {
+			fmt.Println("Done!")
+			break
+		}
+		fmt.Printf("Received %d\n\n", a)
+	}
+}
+
+func main() {
+	c := make(chan int)
+	go countToTen(c)
+	receive(c)
+}
+
+// defer db.Close()
+// cli.Start()
