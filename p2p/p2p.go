8784ed9977c0b490a2f2ef4be36c91a3b77ffb2f
Seong Hoon Wee <mrgravity817@gmail.com>
2021-07-22 09:18:41 +0900

    ADD method to read peer message

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -21,6 +21,7 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
 	initPeer(conn, ip, openPort)
+	conn.WriteMessage(websocket.TextMessage, []byte("Hello from Port 3000!"))
 }
 
 // This will dial(connect) another process to send a message
@@ -29,4 +30,5 @@ func AddPeer(address, port, openPort string) {
 	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandleErr(err)
 	initPeer(conn, address, port)
+	conn.WriteMessage(websocket.TextMessage, []byte("Hello from Port 4000!"))
 }
