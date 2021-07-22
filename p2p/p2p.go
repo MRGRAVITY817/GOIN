b5c1aabecc70e1e7e89fd23aa4773ed1113059cf
Seong Hoon Wee <mrgravity817@gmail.com>
2021-07-22 09:28:06 +0900

    ADD add goroutine to write message using channels

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -3,6 +3,7 @@ package p2p
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/MRGRAVITY817/goin/utils"
 	"github.com/gorilla/websocket"
@@ -20,8 +21,9 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 	}
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
-	initPeer(conn, ip, openPort)
-	conn.WriteMessage(websocket.TextMessage, []byte("Hello from Port 3000!"))
+	peer := initPeer(conn, ip, openPort)
+	time.Sleep(2 * time.Second)
+	peer.inbox <- []byte("Hello from 3000!")
 }
 
 // This will dial(connect) another process to send a message
@@ -29,6 +31,7 @@ func AddPeer(address, port, openPort string) {
 	// Port :4000 is requesting an upgrade from the port :3000
 	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandleErr(err)
-	initPeer(conn, address, port)
-	conn.WriteMessage(websocket.TextMessage, []byte("Hello from Port 4000!"))
+	peer := initPeer(conn, address, openPort)
+	time.Sleep(1 * time.Second)
+	peer.inbox <- []byte("Hello from 4000!")
 }
--- p2p/peer.go
+++ p2p/peer.go
@@ -9,7 +9,8 @@ import (
 var Peers map[string]*peer = make(map[string]*peer)
 
 type peer struct {
-	conn *websocket.Conn
+	conn  *websocket.Conn
+	inbox chan []byte
 }
 
 func (p *peer) read() {
@@ -19,16 +20,25 @@ func (p *peer) read() {
 		if err != nil {
 			break
 		}
-		fmt.Printf("%s", m)
+		fmt.Printf("%s\n", m)
+	}
+}
+
+func (p *peer) write() {
+	for {
+		m := <-p.inbox // this will block until channel gets message to write
+		p.conn.WriteMessage(websocket.TextMessage, m)
 	}
 }
 
 func initPeer(conn *websocket.Conn, address, port string) *peer {
 	p := &peer{
 		conn,
+		make(chan []byte),
 	}
 	key := fmt.Sprintf("%s:%s", address, port)
 	go p.read()
+	go p.write()
 	Peers[key] = p
 	return p
 }
