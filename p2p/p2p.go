52e510e698c47a0602035efc9c5a703876e33812
Seong Hoon Wee <mrgravity817@gmail.com>
2021-07-22 09:41:58 +0900

    ADD method to close when read and write is over, and delete peers when connection closed

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -3,7 +3,6 @@ package p2p
 import (
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/MRGRAVITY817/goin/utils"
 	"github.com/gorilla/websocket"
@@ -21,9 +20,7 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 	}
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
-	peer := initPeer(conn, ip, openPort)
-	time.Sleep(2 * time.Second)
-	peer.inbox <- []byte("Hello from 3000!")
+	initPeer(conn, ip, openPort)
 }
 
 // This will dial(connect) another process to send a message
@@ -31,7 +28,5 @@ func AddPeer(address, port, openPort string) {
 	// Port :4000 is requesting an upgrade from the port :3000
 	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandleErr(err)
-	peer := initPeer(conn, address, openPort)
-	time.Sleep(1 * time.Second)
-	peer.inbox <- []byte("Hello from 4000!")
+	initPeer(conn, address, openPort)
 }
