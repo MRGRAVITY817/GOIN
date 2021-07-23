1c8f6000bee0db2d862823c718abf5eaefe4a0e0
Seong Hoon Wee <mrgravity817@gmail.com>
2021-07-23 09:21:31 +0900

    ADD two more switch cases in p2p.messages.handleMsg()

--- p2p/messages.go
+++ p2p/messages.go
@@ -2,7 +2,6 @@ package p2p
 
 import (
 	"encoding/json"
-	"fmt"
 
 	"github.com/MRGRAVITY817/goin/blockchain"
 	"github.com/MRGRAVITY817/goin/utils"
@@ -36,12 +35,39 @@ func sendNewestBlock(p *peer) {
 	p.inbox <- m
 }
 
+func requestAllBlocks(p *peer) {
+	m := makeMessage(MessageAllBlocksRequest, nil)
+	p.inbox <- m
+}
+
+func sendAllBlocks(p *peer) {
+	m := makeMessage(MessageAllBlocksResponse, blockchain.Blocks(blockchain.Blockchain()))
+	p.inbox <- m
+}
+
 func handleMsg(m *Message, p *peer) {
 	switch m.Kind {
 	case MessageNewestBlock:
 		var payload blockchain.Block
 		utils.HandleErr(json.Unmarshal(m.Payload, &payload))
-		fmt.Println(payload)
+		b, err := blockchain.FindBlock(blockchain.Blockchain().NewestHash)
+		utils.HandleErr(err)
+		if payload.Height >= b.Height {
+			// We have less block! Ask my peer to get all the blocks
+			requestAllBlocks(p)
+		} else {
+			// Seems like our peer is not up to date.
+			// Let's give him/her a newest block.
+			sendNewestBlock(p)
+		}
+	case MessageAllBlocksRequest:
+		// If my peer asks for all blocks,
+		// send my peer all the blocks
+		sendAllBlocks(p)
+	case MessageAllBlocksResponse:
+		// When my peer gave me all the blocks in byte format,
+		// we receive it and unmarshal into block slice type.
+		var payload []*blockchain.Block
+		utils.HandleErr(json.Unmarshal(m.Payload, &payload))
 	}
-	fmt.Printf("Peer: %s, Sent a message with kind of: %d", p.key, m.Kind)
 }
